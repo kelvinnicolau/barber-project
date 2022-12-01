8c4c22d4d88f7c1e941ff2abab342cd3c902ff0c
Kelvin <kelvinnicolau7@gmail.com>
2022-12-01 14:36:48 -0300

    fitch: ajustando metro.config

--- app/metro.config.js
+++ app/metro.config.js
@@ -15,3 +15,29 @@ module.exports = {
     }),
   },
 };
+
+const { getDefaultConfig } = require("expo/metro-config");
+
+module.exports = (() => {
+  const config = getDefaultConfig(__dirname);
+
+  const { transformer, resolver } = config;
+
+  config.transformer = {
+    ...transformer,
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+    babelTransformerPath: require.resolve("react-native-svg-transformer"),
+  };
+  config.resolver = {
+    ...resolver,
+    assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+    sourceExts: [...resolver.sourceExts, "svg"],
+  };
+
+  return config;
+})();
